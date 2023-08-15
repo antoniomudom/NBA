d66dad02ad16a237687db1599888dbaf2f6f99a8
Antonio Muñoz <antoniomunozdominguez@gmail.com>
2023-08-15 14:45:16 +0200

    prueba

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,66 +1,53 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Equipo = require("../models/Team.model.js")
-const { updateLocals } = require("../middlewares/auth.middlewares.js")
-router.use(updateLocals)
-
-
+const Equipo = require("../models/Team.model.js");
+const { updateLocals } = require("../middlewares/auth.middlewares.js");
+router.use(updateLocals);
 
 /* GET home page */
 router.get("/", (req, res, next) => {
   res.render("index");
 });
 
-const authRouter = require("./auth.routes.js")
-router.use("/auth", authRouter)
+const authRouter = require("./auth.routes.js");
+router.use("/auth", authRouter);
 
-const userRouter = require("./user.routes.js")
-router.use("/user", userRouter)
-
+const userRouter = require("./user.routes.js");
+router.use("/user", userRouter);
 
 //routa de conferencias
-router.get("/conferencias",(req,res,next)=>{
+router.get("/conferencias", (req, res, next) => {
   res.render("conferencias");
+});
+router.get("/eastern", (req, res, next) => {
+  res.render("eastern");
+});
 
-})
-router.get("/eastern", (req,res,next)=>{
-  res.render("eastern")
-})
-
-router.get("/western", (req,res,next)=>{
-  res.render("western")
-})
+router.get("/western", (req, res, next) => {
+  res.render("western");
+});
 //Rutas de favorito
 
-router.get("/favorito",(req,res,next)=>{
+router.get("/favorito", (req, res, next) => {
   res.render("favorito");
-
-})
+});
 
 //Ruta de pronósticos
 
 router.get("/pronostico", (req, res, next) => {
-   Equipo.find().select({ nombre: 1 })
-   .then((response)=>{
-    res.render("pronostico.hbs", {
-      allTeams: response,
-
-
-   })
-    
+  Equipo.find()
+    .select({ nombre: 1 })
+    .then((response) => {
+      res.render("pronostico.hbs", {
+        allTeams: response,
+      });
     })
-   .catch ((error) =>{
-    next(error);
-  })
+    .catch((error) => {
+      next(error);
+    });
 });
-router.post("/pronostico",(req,res,next)=>{
+router.post("/pronostico", (req, res, next) => {
   res.redirect("/pronostico-favorito.hbs");
-
-})
-
-
-
-
-
+});
 
 module.exports = router;
